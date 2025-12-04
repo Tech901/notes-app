f6c686d47192b87637cb8aebab0c8e4ad2404271
Brad Montgomery <brad@bradmontgomery.net>
2025-12-03 20:57:41 -0600

    updated for 2025

--- application.py
+++ application.py
@@ -1,57 +1,48 @@
-# check out flask at:
-# https://flask.palletsprojects.com/en/1.1.x/
-from datetime import datetime
-from flask import Flask
-from flask import request
-from flask import jsonify
-from flask import render_template
-from flask import redirect, url_for
+"""
+A simple Flask application for managing notes.
 
-from .notes import Notes
+It uses the same Notes class as our notes.py module for data management.
+
+For more details on Flask, visit: https://flask.palletsprojects.com/en/stable/
+"""
+
+from flask import Flask
+from flask import request, render_template, redirect, url_for
+
+from notes import Notes
 
 app = Flask(__name__)
 
-@app.route('/')  # decorator
+@app.route("/")
 def index():
-    username = "Brad Montgomery"
     notes = Notes()
-    response = render_template(
-        "index.html",
-        name=username,
-        notes=notes.get_notes()
-    )
+    response = render_template("index.html", notes=notes.get_notes())
     notes.close()
     return response
 
-# Add a note
-@app.route("/create", methods=['GET', 'POST'])
-def add_note():
 
+@app.route("/create", methods=["GET", "POST"])
+def add_note():
     if request.method == "POST":
         notes = Notes()
         notes.add_note(request.form["content"])
-        # redirect after POST
-        return redirect(url_for('index'))
-
-    # GET requests...
+        notes.close()
+        # redirect after Post
+        return redirect(url_for("index"))
+    # GET requests
     return render_template("create.html")
 
 
-# TODO: Delete a note
 @app.route("/delete/<int:note_id>", methods=["GET", "POST"])
 def delete_note(note_id):
-    if request.method == "POST":
-        notes = Notes()
-        notes.remove_note(note_id)
-        return redirect(url_for("index"))  # redirect after POST
-
-    # GET request: Show the content of the note...
     notes = Notes()
+    if request.method == "POST":
+        notes.remove_note(note_id)
+        return redirect(url_for("index")) # redirect after POST
+
+    # GET requests
     return render_template("confirm.html", note=notes.get_note(note_id))
 
-
-@app.route("/time")
-def time():
-    t = datetime.now()
-    content = {"time": t.strftime("%c")}
-    return jsonify(content)
+# IDEAS for updates
+# 1. Make a route for a single note, e.g. /<int:note_id>
+# 2. Make an Edit route, e.g. /edit/<int:note_id>
--- notes.py
+++ notes.py
@@ -1,85 +1,112 @@
 """
-This program implements a simple command line
-note-taking app that stores it's data in a SQLite
-database.
+This program implements a simple command-line note-taking
+app that stores its data in an SQLite database.
+
+To create your database, run `sqlite3 notes.db`, then run the following SQL command:
+
+    CREATE TABLE IF NOT EXISTS 'notes' (
+        'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+        'content' TEXT, 
+        'added_on' DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+    CREATE TABLE sqlite_sequence(name,seq);
+
+---- 
 
 """
-# TODO: build a simple web service that lets you use this to add
-# notes from the web.
-
 import argparse
 import sqlite3
+
 
 class Notes:
 
     def __init__(self):
-        self._db_name = "notes.db" # Convention: Private variable.
+        self._db_name = "notes.db"
         self.conn = sqlite3.connect(self._db_name)
         self.cursor = self.conn.cursor()
 
-    def close(self):
+    def close(self):  # Closes our database
         self.cursor.close()
         self.conn.close()
 
-    def get_notes(self):
-        self.cursor.execute("select * from notes")
-        results = self.cursor.fetchall()
-        # [(1, "content", "2020-11-12 12:34:56"), ...]
+    def get_notes(self): # get all of our notes
+        sql = "select * from notes"
+        self.cursor.execute(sql)
+        results = self.cursor.fetchall()  # [(1, 'note here', '2025-12-01...'), ]
         return results
 
-    def print_notes(self):
+    def print_notes(self): # print all of our notes
         for id, content, date in self.get_notes():
-            print(f"[{id}] Note saved on {date}")
-            print(content, end="\n\n")
+            print(f"{id}:{date} {content}")
 
-    def get_note(self, note_id):
-        """Get a single note matching the note_id"""
+    def get_note(self, note_id):  # get a *single* note
         sql = "select * from notes where id = ?"
         values = (note_id, )
         self.cursor.execute(sql, values)
-        tup = self.cursor.fetchone()
+        note = self.cursor.fetchone() # (id, content, date)
         return {
-            "id": tup[0],
-            "content": tup[1],
-            "created_on": tup[2],
+            "id": note[0],
+            "content": note[1],
+            "added_on": note[2],
         }
 
-    def add_note(self, content):
+    def add_note(self, content): # add a *single* note
         sql = "insert into notes (content) values (?)"
-        values = (content, )
+        values = (content, ) # This is a tuple!
         self.cursor.execute(sql, values)
         self.conn.commit()
         print("Note saved.")
 
-    def remove_note(self, id):
-        sql = "delete from notes where id=?"
-        values = (id, )
+    def remove_note(self, note_id): # remove a *single* note
+        sql = "delete from notes where id = ?"
+        values = (note_id, )
         self.cursor.execute(sql, values)
         self.conn.commit()
-        print(f"Note {id} removed")
+        print(f"Note {note_id} was removed")
 
 
+# Main program...
 if __name__ == "__main__":
-    # See the argpase docs at: https://docs.python.org/3/library/argparse.html
-    parser = argparse.ArgumentParser(description='Do stuff with notes')
+
+    parser = argparse.ArgumentParser(description="Do stuff with notes")
     parser.add_argument(
-        '-d', '--delete', action='store', type=int,
+        "-c",
+        "--content",
+        action="store",
+        type=str,
         default=None,
-        help='Specify the ID of the note to delete'
+        help="The content of the note to add."
     )
     parser.add_argument(
-        '-c', '--content', type=str,
+        "-d",
+        "--delete",
+        action="store",
+        type=int,
         default=None,
-        help="The content of the note to add"
+        help="The ID of the note to delete."
     )
+    parser.add_argument(
+        "-n",
+        "--note",
+        action="store",
+        type=int,
+        default=None,
+        help="The ID of the note you want to view."
+    )
+
     args = parser.parse_args()
 
-    # Create a Notes object (or instance)
+    # Create a Notes object
     notes = Notes()
     if args.content is not None:
         notes.add_note(args.content)
     elif args.delete is not None:
         notes.remove_note(args.delete)
+    elif args.note is not None: #show the single note.
+        n = notes.get_note(args.note)
+        print(f"Note ID: {n['id']}")
+        print(n['content'])
+        print(f"Saved on: {n['added_on']}")
     else:
         notes.print_notes()
     notes.close()
